38e8f3e50b627a2a66738753ddc038b7557fc2df
Klesti Selimaj <klesti.selimay@icloud.com>
2026-07-07 00:11:41 +0200

    Fs read

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod code;
 mod parser;
 
 fn main() {
-    let statements = parser::parse("int main();").unwrap();
+    let source = std::fs::read_to_string("cat/main.cat").expect("Failed to read src file");
+
+    let statements = parser::parse(&source).unwrap();
     let mut cg = CodeGen::new();
 
     cg.generate(statements);
